64eb3fadfe2eacb120fb984f0d49b895ab02de75
Carl Hewett <corteocarl@gmail.com>
2020-12-16 05:07:55 -0500

    Removed useless header

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with ResExtractor. If not, see <http://www.gnu.org/licenses/>.
 
-#include "main.hpp"
 #include "ResExtractor.hpp"
 
 #include <cstddef> // For size_t
